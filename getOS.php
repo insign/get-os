fbc0f8d291dc882620977fef955953e0ba3baaba
helio <insign@gmail.com>
2017-01-10 15:54:43 -0300

    Initial release

---
+++ getOS.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: helio
+ * Date: 10/01/17
+ * Time: 15:35
+ */
+class getOS
+{
+   const UNKNOWN = 1;
+   const WIN = 2;
+   const LINUX = 3;
+   const OSX = 4;
+
+   /**
+    * @return int
+    */
+   static public function ofServer()
+   {
+      switch (TRUE) {
+         case stristr(PHP_OS, 'DAR'):
+            return self::OSX;
+         case stristr(PHP_OS, 'WIN'):
+            return self::WIN;
+         case stristr(PHP_OS, 'LINUX'):
+            return self::LINUX;
+         default :
+            return self::UNKNOWN;
+      }
+   }
+}
